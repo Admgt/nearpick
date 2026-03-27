15886bee808c062064605c194ff6f2ed7cf3a4db
Kalman Adam <h373742@stud.u-szeged.hu>
2026-03-27 15:44:58 +0100

    remove unused import

--- mobile/nearpick/lib/core/error/app_error_message.dart
+++ mobile/nearpick/lib/core/error/app_error_message.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import '../auth/auth_error_message.dart';
 
